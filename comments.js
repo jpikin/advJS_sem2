21e141f81c2711ffd7ebd3db6750d50d2a9eb92a
pikin <pikin@bk.ru>
2024-10-02 00:09:27 +0500

    task 2 done

---
+++ comments.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector('.button');
+const ulList = document.querySelector('.comments_list');
+const input = document.querySelector('.input');
+
+
+btn.addEventListener('click', (e) => {
+    const inputText = input.value;
+    
+    try {
+        validator(inputText);
+        const li = document.createElement('li');
+        li.textContent = inputText;
+        ulList.appendChild(li);
+        input.value = '';
+    } catch (error) {
+        console.error(error);
+    }
+    
+});
+
+function validator(text) {
+    if (50 <= text.length && text.length <= 100) {
+        return text;
+    } else {
+        throw new Error(`Длина комментария должна быть от 50 до 100 символов. Сейчас длина комментария ${text.length} символов`);
+    }
+}
